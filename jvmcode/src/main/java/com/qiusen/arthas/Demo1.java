4a33b85766e22021405304966387e3c14bcfbffa
qiusen <2559231099@qq.com>
2024-04-09 20:55:33 +0800

    file commit

---
+++ jvmcode/src/main/java/com/qiusen/arthas/Demo1.java
@@ -0,0 +1,7 @@
+package com.qiusen.arthas;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int i = 1;
+    }
+}
---
+++ jvmcode/src/main/java/com/qiusen/arthas/Demo2.java
@@ -0,0 +1,9 @@
+package com.qiusen.arthas;
+
+public class Demo2 {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            Thread.sleep(2000);
+        }
+    }
+}
